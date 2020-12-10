30aeb6686636c7add756c0123a0ce2de1622b034
ShweSinHtay <shwesin@isgm2.com>
2020-12-10 23:08:58 +0630

    Equality in Hibernate Study

--- src/main/java/domain/Book.java
+++ src/main/java/domain/Book.java
@@ -2,9 +2,20 @@ package domain;
 
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.ManyToAny;
+
+@Entity
 public class Book {
+    private Long id;
     private String title;
     private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book",joinColumns = @JoinColumn(name = "book_id"),inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     public Book() {
@@ -15,7 +26,7 @@ public class Book {
         this.isbn = isbn;
         this.authors = authors;
     }
-
+    
     public String getTitle() {
         return title;
     }
@@ -39,6 +50,45 @@ public class Book {
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [authors=" + authors + ", id=" + id + ", isbn=" + isbn + ", title=" + title + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Book other = (Book) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+    
     
     
     
