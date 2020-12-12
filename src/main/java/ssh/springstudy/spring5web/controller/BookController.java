b564f215931b934a799baa6abf529ce8d11df0b2
ShweSinHtay <shwesin@isgm2.com>
2020-12-12 21:52:45 +0630

    BookController and list.html thymeleaf added

---
+++ src/main/java/ssh/springstudy/spring5web/controller/BookController.java
@@ -0,0 +1,22 @@
+package ssh.springstudy.spring5web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ssh.springstudy.spring5web.repositories.BookRepository;
+
+@Controller
+public class BookController {
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "list";
+    }
+}
